641e385bee7f37d36aa9ab54764b22f2005e100c
Ivan Lazunin <code.wibbn@gmail.com>
2021-06-13 18:53:43 +0300

    edit setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,12 @@ import setuptools
 setuptools.setup(
     name="bestplace",
     packages=setuptools.find_packages(),
-    install_requires=[]
+    install_requires=[
+        'pandas',
+        'numpy',
+        'scipy',
+        'flask',
+        'flask_script',
+        'flask-restx',
+    ]
 )
