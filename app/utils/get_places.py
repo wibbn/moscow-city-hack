bb7c136b25827899703b811dcc665f75d2e490b6
Ivan Lazunin <code.wibbn@gmail.com>
2021-06-14 11:46:42 +0300

    add lands and metros

--- app/utils/get_places.py
+++ app/utils/get_places.py
@@ -24,6 +24,13 @@ fit = {
         'beauty': ['beauty'],
         'retail': ['malls'],
         'services': ['services']
+    },
+    'land': {
+        'cafe': ['метро', 'парк', 'памятник'],
+        'medicine': ['метро', 'жилой комплекс'],
+        'beauty': ['метро', 'жилой комплекс'],
+        'retail': ['метро', 'жилой комплекс'],
+        'services': ['метро']
     }
 }
 
@@ -31,13 +38,15 @@ def get_topk_places(k, type, place=None):
     data = pd.read_csv(basedir + '/data/df.csv', index_col=0)
     bizs = pd.read_csv(basedir + '/data/bizs.csv', index_col=0)
     locs = pd.read_csv(basedir + '/data/locs.csv', index_col=0)
+    lands = pd.read_csv(basedir + '/data/lands.csv', index_col=0)
 
     if place: 
         coords = geocode(place)
 
         data = get_place_arround(data, coords, 0.02)
         bizs = get_place_arround(bizs, coords, 0.02)
-        locs = get_place_arround(locs, coords, 0.02)
+        locs = get_place_arround(locs, coords, 0.01)
+        lands = get_place_arround(lands, coords, 0.03)
 
     locs = locs[locs['type_custom'].isin(fit['fit'][type])]
 
@@ -51,20 +60,32 @@ def get_topk_places(k, type, place=None):
         s['counts'] = (wifi1['counts'] / 28).astype(int)
         s['usertime'] = (wifi1['dur_per_user']).astype(int)
         
-        s['biz_arround'] = get_place_arround(bizs[bizs['type_custom'].isin(fit['good'][type])], x, 0.007)
-        s['comp_arround'] = get_place_arround(bizs[bizs['type_custom'].isin(fit['bad'][type])], x, 0.007)
+        s['biz_arround'] = get_place_arround(bizs[bizs['type_custom'].isin(fit['good'][type])], x, 0.007, return_dist=True)
+        s['comp_arround'] = get_place_arround(bizs[bizs['type_custom'].isin(fit['bad'][type])], x, 0.007, return_dist=True)
+        s['land_arround'] = get_place_arround(lands[lands['type_custom'].isin(fit['land'][type])], x, 0.01, return_dist=True)
         
         s['biz_count'] = s['biz_arround'].shape[0]
         s['comp_count'] = s['comp_arround'].shape[0]
+
+        s['metro_dist'] = 1.5
+        s['metro_station'] = ""
+
+        metros =  s['land_arround']
+        metros = metros[metros['type'] == 'metro']
+        if not metros.empty:
+            metro_idx = metros['place_dist'].argmin()
+            s['metro_dist'] = metros.iloc[metro_idx]['place_dist']
+            s['metro_station'] = metros.iloc[metro_idx]['name']
         
         s['biz_arround'] = list(s['biz_arround'].T.to_dict().values())
         s['comp_arround'] = list(s['comp_arround'].T.to_dict().values())
-        
+        s['land_arround'] = list(s['land_arround'].T.to_dict().values())
+
         return s
 
     df = locs.apply(func, axis=1)
     dfq = pd.concat([locs, df], axis=1)
-    dfq['q'] = df['counts'] + df['usertime'] * 0.8 + df['biz_count'] * 2 - df['comp_count'] * 3
+    dfq['q'] = df['counts'] + df['usertime'] * 0.8 + df['biz_count'] * 2 - df['comp_count'] * 3 - df['metro_dist'] * 100
 
     values = list(dfq.sort_values('q', ascending=False).dropna().drop('q', axis=1).head(k).T.to_dict().values())
 
--- app/utils/utils.py
+++ app/utils/utils.py
@@ -15,5 +15,10 @@ def geocode(address):
 def distance(x, y):
     return ((x['lat']-y['lat'])**2 + (x['lng']-y['lng'])**2)**0.5
 
-def get_place_arround(data, x, dist):
+def get_place_arround(data, x, dist, return_dist=False):
+    if return_dist:
+        d = data.copy()
+        d['place_dist'] = distance(d, x) * 110
+        return d[distance(d, x) <= dist]
+        
     return data[distance(data, x) <= dist]
